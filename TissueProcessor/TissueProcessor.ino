e88ce9f92306427db2d14eec5e2b3cdb0b3c7e67
Bashar Hasan <basharhasan74@gmail.com>
2026-05-14 23:52:28 +0300

    change license to GPL

--- TissueProcessor/TissueProcessor.ino
+++ TissueProcessor/TissueProcessor.ino
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2026 Bashar Hasan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // #define DEBUG // Uncomment to enable Serial output for debugging
 // #define TEST // Uncomment to enable fast timers for testing
 
@@ -1149,7 +1166,7 @@ void verifyWaxConditions()
     }
     if ((s & 2) && !wax2Ready.isActive())
     {
-        lcdShowStatus(F("Critical Error"), F("H1 H2 or S1 S2"));
+        lcdShowStatus(F("Critical Error"), F("H2 or S2"));
         DBGLN("Critical error Tank 12 without melted wax");
         fsm.begin(S_ERROR);
         return;
